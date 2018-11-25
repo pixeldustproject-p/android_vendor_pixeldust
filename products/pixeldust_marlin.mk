d07ec40deb46f87a77cce0179719f8fa43f03864
spezi77 <spezi7713@gmx.net>
2018-11-25 08:15:28 +0100

    marlin/sailfish: Use the correct fp/build desc (PPR2.1005.003.A1).
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/pixeldust_marlin.mk
+++ products/pixeldust_marlin.mk
@@ -37,10 +37,10 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    PRIVATE_BUILD_DESC="marlin-user 9 PQ1A.181105.017.A1 5081125 release-keys"
+    PRIVATE_BUILD_DESC="marlin-user 9 PPR2.181005.003.A1 5108978 release-keys"
 
-BUILD_FINGERPRINT="google/marlin/marlin:9/PQ1A.181105.017.A1/5081125:user/release-keys"
-BUILD_THUMBPRINT="9/PQ1A.181105.017.A1/5081125:user/release-keys"
+BUILD_FINGERPRINT="google/marlin/marlin:9/PPR2.181005.003.A1/5108978:user/release-keys"
+BUILD_THUMBPRINT="9/PPR2.181005.003.A1/5108978:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="spezi77"
