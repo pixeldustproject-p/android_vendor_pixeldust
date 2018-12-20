LineageOS/android_vendor_lineage

soong_config: Allow process-specific override of target SDK version
Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19

build/soong/soong_config.mk
@@ -4,6 +4,7 @@ _contents := $(_contents)    "Lineage":{$(newline)
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Needs_text_relocations, $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)))
 $(call add_json_str, Specific_camera_parameter_library, $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),,true))
 $(call add_json_bool, Uses_nvidia_enhancements, $(filter TRUE,$(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)))

pixeldustproject-p/android_vendor_pixeldust

Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

build/soong/soong_config.mk
@@ -16,6 +16,7 @@ $(call add_json_bool, Target_uses_qsml,                      $(filter true,$(if 
 $(call add_json_bool, Target_uses_eigen,                     $(filter true,$(if $(strip $(TARGET_USES_QSML)),false,true)))
 $(call add_json_bool, Target_use_sdclang,                    $(filter true,$(if $(strip $(TARGET_USE_SDCLANG)),true,false)))
 
+$(call add_json_str_omitempty, Target_process_sdk_version_override, $(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str,  Specific_camera_parameter_library,     $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
 $(call add_json_str,  Libart_img_base,                       $(LIBART_IMG_BASE))
 $(call add_json_str,  QTIAudioPath,                          $(call project-path-for,qcom-audio))