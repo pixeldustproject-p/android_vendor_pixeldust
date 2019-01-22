689d332c8b78cc4fe617dfe7dac24c6487dd0720
spezi77 <spezi7713@gmx.net>
2019-01-23 00:01:49 +0100

    Add bootanimation specially adapted for blueline. Thanks very much @RogerdotT
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/bootanimation.mk
+++ configs/bootanimation.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Add Pixel Dust ROM bootanimation based on device
-ifneq ($(filter pixeldust_blueline pixeldust_angler pixeldust_marlin pixeldust_sailfish pixeldust_taimen,$(TARGET_PRODUCT)),)
+ifneq ($(filter pixeldust_angler pixeldust_marlin pixeldust_sailfish pixeldust_taimen,$(TARGET_PRODUCT)),)
     PRODUCT_COPY_FILES += \
         vendor/pixeldust/prebuilt/bootanimation/1440x2560.zip:system/media/bootanimation.zip
 endif
@@ -21,4 +21,7 @@ ifneq ($(filter pixeldust_bullhead pixeldust_mako pixeldust_Z00L pixeldust_mido,
     PRODUCT_COPY_FILES += \
         vendor/pixeldust/prebuilt/bootanimation/1080x1920.zip:system/media/bootanimation.zip
 endif
-
+ifneq ($(filter pixeldust_blueline,$(TARGET_PRODUCT)),)
+    PRODUCT_COPY_FILES += \
+        vendor/pixeldust/prebuilt/bootanimation/1440x2960.zip:system/media/bootanimation.zip
+endif
