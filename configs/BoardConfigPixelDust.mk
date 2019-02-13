2064fd505c442782834b7d0986d53b942e64e981
Michael Bestas <mkbestas@lineageos.org>
2019-02-13 08:19:20 +0000

    PixelDust: Set TARGET_CRYPTFS_HW_PATH if HW crypto is enabled
    * So TWRP can use it
    
    Change-Id: Ibc1d9d943d6d4e4e5248f623cd06782335f8ae98
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

--- configs/BoardConfigPixelDust.mk
+++ configs/BoardConfigPixelDust.mk
@@ -1,3 +1,8 @@
+# QCOM HW crypto
+ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
+endif
+
 include vendor/pixeldust/configs/BoardConfigKernel.mk
 
 include vendor/pixeldust/configs/BoardConfigSoong.mk
